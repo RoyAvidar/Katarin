f9905fd56d6ae2eaf41bc39bce446ef34a9609c3
Roy Avidar <Roi981av@gmail.com>
2022-01-11 14:11:03 +0200

    Few changes in app structure

---
+++ lib/screens/utils/Background.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:katarin/screens/BaseWidget.dart';
+
+class Background extends BaseWidget {
+  // SpriteComponent _bgSprite;
+
+  // Background(String src) {
+  // _bgSprite = SpriteComponent.fromImage(0, 0, Sprite(src));
+  // }
+
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void resize(Size size) {
+    // TODO: implement resize
+  }
+
+  @override
+  void update() {
+    // TODO: implement update
+  }
+}
